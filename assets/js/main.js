28af5df0b83ca48bca6fdf062c96117ad2cdee06
Roger Perez <rogelio_perez_98@hotmail.com>
2021-09-19 13:53:42 -0500

    Seccion de skills terminada

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,39 @@ const linkAction = () =>{
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n. addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.querySelectorAll('.skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
 
+// const toggleSkills = (skill) => {
+//     // Traigo la clase de su padre
+//     let itemClass = skill.parentNode;
+//     console.log(itemClass.className)
+//     if (itemClass.className === 'skills__content skills__close') {
+//         itemClass.className = 'skills__content skills__open';
+//     }else{
+//         itemClass.className = 'skills__content skills__close';
+//     }
+// }
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className;
+
+    for (let index = 0; index < skillsContent.length; index++) {
+        skillsContent[index].className = 'skills__content skills__close';
+    }
+
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+        console.log('entre')
+    }
+}
+
+skillsHeader.forEach( (skill) => {
+    skill.addEventListener( 'click', toggleSkills)
+});
 
 /*==================== QUALIFICATION TABS ====================*/
 
