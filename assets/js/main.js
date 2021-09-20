152e3c9f9a406e5011efdf323376278200ef18df
Roger Perez <rogelio_perez_98@hotmail.com>
2021-09-20 15:04:20 -0500

    Seccion del footer terminada, scroll section active link y background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,10 +103,34 @@ tabs.forEach( tab => {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+// Obtengo todas las secciones
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    // Obtengo la pocicion en y donde se encuentra el user
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current =>{
+        // Obtengo la altura de la seccion
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
 
