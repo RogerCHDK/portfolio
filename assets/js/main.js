3a1e322826addd3c545923c04673e857e3471135
Roger Perez <rogelio_perez_98@hotmail.com>
2021-09-19 19:55:56 -0500

    Seccion de portafolio primera version terminada

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,7 +86,18 @@ tabs.forEach( tab => {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+    let swiper = new Swiper('.portfalio__container', {
+      cssMode: true,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+    });
 
 /*==================== TESTIMONIAL ====================*/
 
